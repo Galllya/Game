0ec2b4c91eb386aef351786ebdc14aa856156aa8
Galya Bolganova <70851177+Galllya@users.noreply.github.com>
2021-05-01 21:05:23 +0700

    game

---
+++ Lab_10/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_10
+{
+
+    public partial class Form1 : Form
+    {
+        Game game = new Game();
+        public int grany;
+        public bool cheat;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            grany = Convert.ToInt32(textBox1.Text);
+            cheat = checkBox1.Checked;
+            game.Start(grany, cheat);
+            label1.Text = "Вам выпало: " + game.player.ToString();
+            label2.Text = "Компьютеру выпало: " + game.iu.ToString();
+            label3.Text = game.Who_win();
+        }
+    }
+}
---
+++ Lab_10/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    public class Game
+    {
+        Random rnd = new Random();
+        public int player;
+        public int iu;
+        double l;
+
+
+        public Boolean Start(int graney, Boolean cheat)
+        {
+            double[] nums_iu = new double[graney+2];
+            Boolean player_win = true;
+            if (cheat == false)
+            {
+                int j = graney / 2;
+                for (int i = 1; i < j; i++)
+                {
+                    l = 0.4 / j;
+                    l = Math.Round(l, 2);
+                    nums_iu[i] = l;
+
+                }
+
+                for (int i = j; i <= graney + 1; i++)
+                {
+                    l = 0.6 / j;
+                    l = Math.Round(l, 2);
+                    nums_iu[i] = l;
+
+                }
+                double value = rnd.NextDouble();
+                Math.Round(value, 2);
+                double A = value;
+                int k = 0;
+                while (A > 0)
+                {
+                    A = A - nums_iu[k];
+                    k++;
+                }
+
+                k--;
+                iu = k;
+            }
+            else
+            {
+                iu = YY(graney);
+            }
+      
+
+           
+
+            
+
+            player = YY(graney);
+
+            return player_win;
+        }
+
+        public string Who_win()
+        {
+            string who_win = "";
+            if (player>iu)
+            {
+                who_win = "Вы выиграли!";
+            }
+            else
+            {
+                if (player==iu)
+                {
+                    who_win = "Ничья!";
+                }
+                else
+                {
+                    who_win = "Вы проиграли!";
+                }
+            }
+            return who_win;
+        }
+
+        public int YY(int graney)
+        {
+            double value = rnd.NextDouble();
+            Math.Round(value, 2);
+            double A = value;
+            int k = 0;
+            while (A >= 0)
+            {
+                l = 0.9 / graney;
+                l = Math.Round(l, 2);
+                A = A - l;
+                k++;
+            }
+
+            k--;
+            return k;
+        }
+        
+     
+    }
+}
+
